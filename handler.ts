f9d91c4db369616022ab0c31af99c8e3d6baf46c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-23 18:35:29 +0900

    とりあえず実行できそうな環境を整えてみました

--- handler.ts
+++ handler.ts
@@ -1,7 +1,13 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as line from '@line/bot-sdk'
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const received: APIGatewayProxyHandler = async (event, _context) => {
+  const lineClient = new line.Client({
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  });
+  console.log(event);
   return {
     statusCode: 200,
     body: JSON.stringify({
---
+++ user-status.ts
@@ -0,0 +1,4 @@
+export enum UserStatus{
+  follow = 0,
+  unfollow = 1,
+}
